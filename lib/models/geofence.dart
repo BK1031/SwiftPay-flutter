9c9e71c2d9ee32c0507e24c0f852be10179fa045
Bharat Kathi <bharat1031@gmail.com>
2019-02-24 11:28:44 -0800

    Added AutoPay w/ Geofencing

---
+++ lib/models/geofence.dart
@@ -0,0 +1,10 @@
+class Geofence {
+  String id;
+  String name;
+  double distance;
+  double price;
+  double lat;
+  double long;
+
+  Geofence(this.id, this.name, this.distance, this.price, this.lat, this.long);
+}
